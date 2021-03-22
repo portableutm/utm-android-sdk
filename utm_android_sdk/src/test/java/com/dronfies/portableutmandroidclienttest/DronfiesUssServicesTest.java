022342565a5e79f5e83e3c185b3d1a4052aa9541
Renzo Beux Valarin <renzo.beux@fing.edu.uy>
2021-03-22 12:14:05 -0300

    adding jsonTest

--- utm_android_sdk/src/test/java/com/dronfies/portableutmandroidclienttest/DronfiesUssServicesTest.java
+++ utm_android_sdk/src/test/java/com/dronfies/portableutmandroidclienttest/DronfiesUssServicesTest.java
@@ -1,15 +1,34 @@
 package com.dronfies.portableutmandroidclienttest;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DronfiesUssServicesTest {
 
-    @BeforeEach
-    void setUp() {
+    private MockWebServer server = new MockWebServer();
+    private static String readFileAsString(String file)throws Exception
+    {
+        return new String(Files.readAllBytes(Paths.get(file)));
+    }
 
+    @BeforeAll
+    void setUp() {
+        try {
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
@@ -54,6 +73,16 @@ class DronfiesUssServicesTest {
 
     @Test
     void getOperations() {
+        String file = "src/test/resources/testOperations.json";
+        try {
+            String json = readFileAsString(file);
+             MockResponse response = new MockResponse();
+             response
+                     .setBody(json);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
